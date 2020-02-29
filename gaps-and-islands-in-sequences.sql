8dea6bc970ebe4175e03acfff45ebb7a8883d06f
Nikolay Sokolov <nikolayssokolov@gmail.com>
2020-02-29 15:36:54 +0300

    Create gaps-and-islands-in-sequences.sql

---
+++ gaps-and-islands-in-sequences.sql
@@ -0,0 +1,17 @@
+-- Find three consecutive numbers in a sequence
+-- gaps-and-islands-in-sequences
+
+with t(id) as (
+    values (1), (2), (2), (3), (3), (3), (2), (4)
+), t_rn as (
+    select id, row_number() over() rn from t
+), t_grp as (
+    select id, rn,
+           rn - row_number() over(partition by id order by rn) grp_id
+      from t_rn
+)
+ select id
+   from t_grp
+  group by id, grp_id
+  having count(*) = 3
+;
