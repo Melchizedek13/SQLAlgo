6877716c5b7a9b78e8c2a818d29349631fcacfbc
Nikolay Sokolov <nikolayssokolov@gmail.com>
2020-03-07 22:40:08 +0300

    Update find_the_latest_sales_of_goods.sql

--- find_the_latest_sales_of_goods.sql
+++ find_the_latest_sales_of_goods.sql
@@ -21,3 +21,15 @@ select t1.item_id, t1.price, t1.dt
 where t2.item_id is null;
 
 -- Oracle way
+with item_sales(item_id, price, dt) as (
+  select 1, 100, date'2020-03-01' from dual union all
+  select 1, 300, date'2020-03-04' from dual union all
+  select 1, 200, date'2020-03-07' from dual union all
+  select 2, 150, date'2020-03-06' from dual
+)
+select item_id,
+       min(price) keep (dense_rank first order by dt desc) price,
+       max(dt) dt
+  from item_sales
+ group by item_id
+;
