9cfe14edc6834c6047f54bd076002bb34d05d899
Nikolay Sokolov <nikolayssokolov@gmail.com>
2020-07-26 17:55:15 +0300

    Added  corner cases based on null & empty values.
    Added  corner cases based on null & empty values.

--- collapsing_identical_rows_by_keys.sql
+++ collapsing_identical_rows_by_keys.sql
@@ -63,3 +63,59 @@ select *
  where startflag = 1
  order by id, load_dt
 ;
+
+/*
+   Same as above. But taking into account blank(empty) lines and NULL values.
+*/
+
+-- Postgres
+with t(id, c1, c2, c3, dml_type, load_dt) as (values
+     (1, '',   null, 'A1', 'I', date'2019-12-28'),
+     (1, null, '',   'B1', 'U', date'2019-12-29'), -- Collapse
+     (1, 'A1', null, 'B2', 'U', date'2019-12-30'),
+     (1, null, 'B3', 'B3', 'U', date'2019-12-31'),
+     (1, 'A1', 'B1', 'C1', 'U', date'2020-01-01'),
+     (1, 'A1', 'B1', 'C2', 'U', date'2020-01-02'), -- Collapse
+     (1, 'A1', 'B1', 'C3', 'U', date'2020-01-03'), -- Collapse
+     (1, 'A1', 'B2', 'C3', 'U', date'2020-01-04'),
+     (1, 'A1', 'B1', 'C3', 'U', date'2020-01-05')
+), agg as (
+    select id, c1, c2, dml_type, load_dt,
+           case when array[row(id, nullif(c1, ''), nullif(c2, ''))]
+                     = lag(array[row(id, nullif(c1, ''), nullif(c2, ''))])
+                         over (order by load_dt)
+                then 0
+                else 1
+           end StartFlag
+      from t
+)
+select *
+  from agg
+ where startflag = 1
+ order by id, load_dt
+;
+
+-- Exasol
+with t(id, c1, c2, c3, dml_type, load_dt) as (values
+     (1, '',   null, 'A1', 'I', date'2019-12-28'),
+     (1, null, '',   'B1', 'U', date'2019-12-29'), -- Collapse
+     (1, 'A1', null, 'B2', 'U', date'2019-12-30'),
+     (1, null, 'B3', 'B3', 'U', date'2019-12-31'),
+     (1, 'A1', 'B1', 'C1', 'U', date'2020-01-01'),
+     (1, 'A1', 'B1', 'C2', 'U', date'2020-01-02'), -- Collapse
+     (1, 'A1', 'B1', 'C3', 'U', date'2020-01-03'), -- Collapse
+     (1, 'A1', 'B2', 'C3', 'U', date'2020-01-04'),
+     (1, 'A1', 'B1', 'C3', 'U', date'2020-01-05')
+), agg as (
+    select id, c1, c2, dml_type, load_dt,
+           case when hash_md5(id, c1, c2) = lag(hash_md5(id, c1, c2)) over (order by load_dt)
+                then 0
+                else 1
+           end StartFlag
+      from t
+)
+select *
+  from agg
+ where startflag = 1
+ order by id, load_dt
+;
