cfbcd82277ec88b4c3b1d67a7ba2973b6817e5d7
Nikolay Sokolov <nikolayssokolov@gmail.com>
2024-01-21 05:15:55 +0400

    Update pg_window_func_how_to_ignore_nulls.sql

--- pg_window_func_how_to_ignore_nulls.sql
+++ pg_window_func_how_to_ignore_nulls.sql
@@ -1,8 +1,5 @@
--- https://stackoverflow.com/questions/18987791/how-do-i-efficiently-select-the-previous-non-null-value
--- https://dbfiddle.uk/jG8iLU-9
-
 /*
-There is a table in Postgres that looks like this:
+There is a table in DBMS that looks like this:
 
  id | dt         | val
 ----+------------+---
@@ -23,6 +20,10 @@ Needs to query it to make it look like this:
 | 2 | 2021-02-03 | x |
 
 */
+
+-- Postgresql
+-- https://stackoverflow.com/questions/18987791/how-do-i-efficiently-select-the-previous-non-null-value
+-- https://dbfiddle.uk/jG8iLU-9
 
 with
 /*
@@ -49,5 +50,24 @@ select id, dt, val, vp,
        first_value(val) over (partition by id, vp)
           as val_pnnv
   from rs
+ order by id, dt
+;
+
+
+-- Oracle
+-- https://dbfiddle.uk/gX09QA5B
+with
+ds(id, dt, val) as (values
+   (1, date'2020-01-01', 'a'),
+   (1, date'2020-02-01', null),
+   (1, date'2020-03-01', 'b'),
+   (2, date'2021-01-01', 'x'),
+   (2, date'2021-02-03', null)
+)
+select id, dt, val,
+       last_value(val) ignore nulls
+          over (partition by id order by dt)
+             as val_pnnv
+  from ds
  order by id, dt
 ;
